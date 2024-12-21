8da26486e495dd6f53faddcb154f8da726072b65
mohsenkamini <mohsenkamini@gmail.com>
2024-12-21 18:41:56 +0330

    created the profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -27,9 +27,8 @@ export default function ProfilePage() {
 				setLoading(false);
 				return;
 			}
-		} 
 			try {
-				const response = await fetch(`${process.env.NET_PUBLIC_API_URL}/api/users/`, {
+				const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/`, {
 					headers: {
 						Authorization: `Bearer ${token}`,
 					},
@@ -37,8 +36,67 @@ export default function ProfilePage() {
 				if (!response.ok) {
 					throw new Error("failed to fetch profile");
 				}
-				const data = await 
+				const data = await response.json();
+				setUser(data);
+			} catch (error) {
+				console.error("Error fetching user profile", error);
 			}
+			setLoading(false);
+		};
+
+		fetchUserProfile();
+	}, []);
+	if (loading) {
+		return (
+			<Container maxWidth="xs" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} >
+				<CircularProgress />
+			</Container>
+		);
 	}
-	)
+	if (!user) {
+		return (
+			<Container maxWidth="xs">
+			  <Box sx={{textAlign: 'center', mt: 2}}>
+			    <Typography variant="h5" color="error">
+				مشکلی در بارگذاری پروفایل پیش آمد
+			    </Typography>
+			  </Box>
+			</Container>
+		);
+	}
+	return (
+		<Container>
+		  <Box>
+		    <Paper>
+			<Typography variant="h4" gutterBottom>
+				پروفایل کاربری
+			</Typography>
+
+			<Typography variant="h6" sx={{mt: 2}}>
+				نام کاربری: {user.username}
+			</Typography>
+			<Typography variant="h6" sx={{mt: 1}}>
+				ایمیل: {user.email || 'اطلاعاتی وارد نشده است.'}
+			</Typography>
+			<Typography variant="h6" sx={{mt: 1}}>
+				بیوگرافی: {user.bio || 'اطلاعاتی وارد نشده است.'}
+			</Typography>
+			<Typography variant="h6" sx={{mt: 1}}>
+				تاریخ تولد: {user.birthday || 'اطلاعاتی وارد نشده است.'}
+			</Typography>
+			{user.profilePicture && (
+				<Box sx={{ mt: 3, textAlign: 'center' }}>
+					<img
+					src={`${process.env.NEXT_PUBLIC_API_URL}${user.profilePicture}`}
+					alt="Profile"
+					width={200}
+					height={200}
+					style={{ borderRadius: '50%'  }}
+					/>
+				</Box>
+			)}
+		    </Paper>
+		  </Box>
+		</Container>
+	);
 }
