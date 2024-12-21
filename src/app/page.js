484b59725319d1963e5450ecb322f3ab6d16995c
mohsenkamini <mohsenkamini@gmail.com>
2024-12-21 19:44:18 +0330

    adding the player functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,58 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Box, Typography, Button, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import { PlayArrow, Pause SkipNext, SkipPrevious, ThumbUp } from "@mui/icons-material";
 import Link from 'next/link';
-import {Box} from '@mui/material';
+
+
+
 export default function Home() {
+  const [stations, setStations] = useState([]);
+  const [currentStation, setCurrentStation] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const [audio, setAudio] = useState(null);
+  useEffect(() => {
+	  const fetchStations = async () => {
+		try {
+			const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL/api/radio/stations}`);
+			if (!response.ok) throw new Error("Failed to fetch stations");
+			const data = await response.json();
+			setStations(data);
+		} catch (error) {
+			console.error("Error fetching stations:", error);
+		}
+	  };
+
+	  fetchStations();
+  }, []);
+  const handlePlay = async (station) => {
+	if (currentStation !== station) {
+	  setCurrentStation(station);
+	}
+ 	try {
+		const response = await fetch(
+		  `${process.env.NEXT_PUBLIC_API_URL}/api/radio/stations/${station.id}/play/`
+		);
+		if (!response.ok) throw new Error ("Failed to fetch song");
+		const song = await respose.json();
+		setCurrentSong(song);
+
+		if (audio) {
+		  audio.pause();
+		}
+		const newAudio = new Audio(`${process.env.NEXT_PUBLIC_API_URL}${song.url}`);
+		newAudio.play();
+		setAudio(newAudio);
+		setIsPlaying(True);
+
+		newAudio.addEventListener("ended", () => setIsPlaying(false));
+	} catch (error) {
+	  console.error("Error playing song:", error)
+	} 
+  };
+
   return (
     <Box>
       <h1>رادیو بهشت</h1>
