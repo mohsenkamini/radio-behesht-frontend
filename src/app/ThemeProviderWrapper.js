c14d3ca21484fbd4de67fcee0f67da920f373217
mohsenkamini <mohsenkamini@gmail.com>
2024-12-29 10:42:57 +0330

    added parts that we can switch to dark mode in the code(for now)

--- src/app/ThemeProviderWrapper.js
+++ src/app/ThemeProviderWrapper.js
@@ -10,9 +10,63 @@ const cacheRtl = createCache({
 });
 
 const rtlTheme = createTheme({
+  //palette: {
+  //      mode: "dark",
+  //      background: {
+  //        default: "#121212",
+  //        paper: "1e1e1e",
+  //      },
+  //      text: {
+  //        primary: "#ffffff",
+  //        secondary: "b3b3b3",
+  //      },
+  //      primary: {
+  //        main: "#007BFF",
+  //      },
+  //},
   direction: "rtl",
   typography: {
     fontFamily: "'Vazirmatn', 'Roboto', 'Arial', sans-serif",
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px', // Rounded buttons
+          padding: '8px 16px',
+          boxShadow: 'none', // Remove button shadows
+          '&:hover': {
+            backgroundColor: '#0056b3', // Darker blue on hover
+          },
+        },
+      },
+    },
+    MuiList: {
+      styleOverrides: {
+        root: {
+          //border: '1px solid #3C3C3C', // Border for station list
+          borderRadius: '8px',
+          padding: '8px',
+        },
+      },
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          color: '#FFFFFF', // White text
+          '&.Mui-selected': {
+            backgroundColor: '#007BFF', // Blue background for selected station
+            color: '#FFFFFF', // White text for selected station
+            '&:hover': {
+              backgroundColor: '#0056b3', // Darker blue on hover for selected
+            },
+          },
+          '&:hover': {
+            backgroundColor: '#3C3C3C', // Slight hover effect for list items
+          },
+        },
+      },
+    },
   },
 });
 
